7b4d58baed7834bf03c1d1f69ba72419af735102
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2023-03-22 15:00:03 -0300

    chore: add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import { categoryRoutes } from './routes/categoryRoutes.js'
 import { productRoutes } from './routes/productRoutes.js'
 import { generateToken } from './utils/generateToken.js'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.get('/auth', (_request, response) => {
   const accessToken = generateToken({ isAdmin: true })
